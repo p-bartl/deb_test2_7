1f6880c71586c5067aa8cfd28c45b2ab9f4c8756
p-bartl <pascal.bartl@bmw.de>
2021-11-25 23:10:38 +0100

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ZooComponent } from './zoo/zoo.component';
 import { ZooAnimalComponent } from './zoo-animal/zoo-animal.component';
+//import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { ZooAnimalComponent } from './zoo-animal/zoo-animal.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
